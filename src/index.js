2361be58f1430e7b383cc92f9b0a38df7ed01f5e
Saptgiri7 <saptgiriyangal@gmail.com>
2026-03-01 18:48:38 +0530

    implemented matches get and post routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { matchRouter } from './routes/matches.js';
+import { db } from './db/db.js';
 const port = 8000;
 
 const app = express();
@@ -10,6 +12,7 @@ app.get('/',(req,res)=>{
     res.send('<h1>Hello from express server</h1>')
 })
 
+app.use('/matches',matchRouter);
 
 app.listen(port,()=>{
     console.log(`server listening on port: ${port}`);
